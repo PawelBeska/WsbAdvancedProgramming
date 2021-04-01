acef0b5092760dd047f8ceed36dec2a4ae58a5fc
pawel <raz4r@interia.pl>
2021-04-01 09:43:18 +0200

    Updating index page
    [CHANGE] Changing content of the index page #16

--- resources/views/home/components/index/index.blade.php
+++ resources/views/home/components/index/index.blade.php
@@ -3,13 +3,14 @@
     <div class="col-md-12">
         <div class="card">
             <div class="card-header">
-                Quote
+                COVID
             </div>
             <div class="card-body">
-                <blockquote class="blockquote mb-0">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-                    <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
-                </blockquote>
+                Pandemia COVID-19 – pandemia zakaźnej choroby COVID-19 wywoływanej przez koronawirusa SARS-CoV-2. Rozpoczęła się jako epidemia 17 listopada 2019 w mieście Wuhan, w prowincji Hubei, w środkowych Chinach, a 11 marca 2020 została uznana przez Światową Organizację Zdrowia (WHO) za pandemię.
+
+                W okresie od listopada 2019 do stycznia 2020 zachorowania pojawiały się głównie w mieście Wuhan, w środkowych Chinach, ale już w połowie stycznia wirus rozprzestrzenił się w całych Chinach. W drugiej połowie lutego ogniska zakażeń z setkami chorych wybuchły w Korei Południowej, we Włoszech oraz w Iranie. Od 4 marca 2020 są notowane zakażenia wirusem SARS-CoV-2 w Polsce. 13 marca 2020 WHO podała, że centrum pandemii koronawirusa stała się Europa. Później zachorowania rozlały się na pozostałe kontynenty, a ostatnim, na którym stwierdzono zachorowania, stała się w grudniu 2020 Antarktyda.
+
+            <footer class="blockquote-footer">Źródło: <a href="https://pl.wikipedia.org/wiki/Pandemia_COVID-19">Wikipedia</a></footer>
             </div>
         </div>
     </div>
